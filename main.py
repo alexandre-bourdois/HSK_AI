b2d9725379eafada697e871645197729a8239102
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2024-02-08 21:39:20 +0100

    Test model

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 train_data_dir = '../dataset_chinese/train'
 test_data_dir = '../dataset_chinese/test'
 
-sample_size = 32
+sample_size = 64
 img_height, img_width = 67, 67 
 
 #Generate characters
@@ -37,26 +37,31 @@ test_generator = test_data_gen.flow_from_directory(
 )
 
 # create model
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(img_height, img_width, 3)))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=178, activation=tf.nn.softmax))
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Flatten(input_shape=(img_height, img_width, 3)))
+# model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+# model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+# model.add(tf.keras.layers.Dense(units=178, activation=tf.nn.softmax))
 
-model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(train_generator, epochs=1, validation_data=test_generator)
+# model.fit(train_generator,validation_data=test_generator, epochs=1 )
 
-accuracy, loss = model.evaluate(train_generator, test_generator)
-print(accuracy)
-print(loss)
+# accuracy, loss = model.evaluate(train_generator, test_generator)
+# print(accuracy)
+# print(loss)
 
-model.save('digits.model')
+# model.save('digits.model')
 
-img= cv.imread('bu.png')[:,:,0]
-img= np.invert(np.array([img]))
+# Charger une image et la pretraiter
+img_path = 'bu.png'
+img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
+img = cv.resize(img, (img_height, img_width))
+img = np.invert(img) / 255.0  # Inverser l'image et normaliser les valeurs des pixels
+img = np.expand_dims(img, axis=0)  # Ajouter une dimension pour le lot (batch)
 
-# loaded_model = tf.keras.models.load_model('digits.model')
+
+model = tf.keras.models.load_model('digits.model')
 
 prediction = model.predict(img)
 print(f'The result is probably:{ np.argmax(prediction)}')
