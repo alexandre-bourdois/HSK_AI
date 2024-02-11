2e671f24246e2adfbf7c5fe2aaf47b84ad806203
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2024-02-11 13:33:47 +0100

    load_test_character function

--- main.py
+++ main.py
@@ -11,21 +11,32 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Function to create the model
-def create_model(img_height, img_width, num_classes):
+def create_model(img_height, img_width, num_classes,train_generator,test_generator):
+    train_generator=tf.keras.utils.normalize(train_generator, axis=1)
+    test_generator=tf.keras.utils.normalize(test_generator, axis=1)
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(img_height, img_width, 3)),
+        tf.keras.layers.Flatten(input_shape=(img_height, img_width,      1)),
         tf.keras.layers.Dense(units=128, activation=tf.nn.relu),
         tf.keras.layers.Dense(units=128, activation=tf.nn.relu),
         tf.keras.layers.Dense(units=num_classes, activation=tf.nn.softmax)
     ])
+    model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(train_generator,validation_data=test_generator, epochs=2 )
+    model.save('character.model')
     return model
+
+def load_test_character(img_path):
+    img = cv.imread(img_path)
+    img = cv.resize(img, (67, 67))
+    img = np.expand_dims(img, axis=0) 
+    img = img / 255.0  
+    return img
 
 #Repertory of the dataset
 train_data_dir = '../dataset_chinese/train'
 test_data_dir = '../dataset_chinese/test'
 
-sample_size = 64
+sample_size = 32
 img_height, img_width = 67, 67 
 num_classes=178
 
@@ -47,25 +58,20 @@ test_generator = test_data_gen.flow_from_directory(
     class_mode='sparse'
 )
 
-# Create and compile the model
-model = create_model(img_height, img_width, num_classes)
-model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(train_generator,validation_data=test_generator, epochs=1 )
 
-model.save('numbers.model')
+# Create, compile and save the model
+# model = create_model(img_height, img_width, num_classes,train_generator,test_generator)
 
 # Load the trained model
-#model = tf.keras.models.load_model('numbers.model')
+model = tf.keras.models.load_model('character.model')
 
 
-img_path = 'yi.png'
-img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
-img = cv.resize(img, (img_height, img_width))
-img = np.invert(img) / 255.0  # Inverser l'image et normaliser les valeurs des pixels
-img = np.expand_dims(img, axis=0)  # Ajouter une dimension pour le lot (batch)
+img = load_test_character('yi.png')
 
 
 prediction = model.predict(img)
-print(f'The result is probably:{ np.argmax(prediction)}')
+predicted_class_index = np.argmax(prediction)
+predicted_class_name = list(train_generator.class_indices.keys())[predicted_class_index]
+print(f"The predicted character is: {predicted_class_name}")
 plt.imshow(img[0],cmap=plt.cm.binary)
 plt.show()
