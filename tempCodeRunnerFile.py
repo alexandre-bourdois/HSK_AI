cef65d5d80dd2961d375751d0ef92988d980ea6a
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2024-02-16 21:09:16 +0100

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-model.add(tf.keras.layers.Flatten(input_shape=(img_height, img_width, 3)))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=178, activation=tf.nn.softmax))
